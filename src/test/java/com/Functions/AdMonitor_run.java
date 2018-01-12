e9cd78dc37c4e0f6bc5edbc1f7cd4f1dee87b505
naresh.kumar.contractor@weather.com <naresh.kumar.contractor@weather.com>
2018-01-12 15:53:17 +0530

    added try cach for two methds
    added try cach for two methds when crash app

--- src/test/java/com/Functions/AdMonitor_run.java
+++ src/test/java/com/Functions/AdMonitor_run.java
@@ -20,119 +20,162 @@ import org.testng.asserts.SoftAssert;
 import com.Genaral.Driver;
 import com.Genaral.readExcelValues;
 
-public class AdMonitor_run extends Driver {
+//---Main method for execute the test
 
-	public static String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmm").format(Calendar.getInstance().getTime());
+/**
+ * @author Naresh
+ *
+ */
+public class AdMonitor_run extends Driver {
+	
+
+	//public static String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmm").format(Calendar.getInstance().getTime());
 	
 
 	//	@Test(priority=0)
 	//	public static void Feed_0_Ad_VAlidation() throws Exception{
 	//		Functions.Feed_Ad_Validation(0);
 	//	}
+	
+	
 	@Test(priority=1)
-	public static void Feed_1_Ad_Validation() throws Exception {
+	//Execute method for feed_1 Validation
+	public void feed_1_Ad_Validation() throws Exception {
 		Functions.Feed_Ad_Validation(1);
 	}
-
+	
 	@Test(priority=2)
-	public static void Feed_2_Ad_VAlidation() throws Exception{
+	//Execute method for feed_2 Validation
+	public void feed_2_Ad_VAlidation() throws Exception{
 		Functions.Feed_Ad_Validation(2);
 	}
+	
 	@Test(priority=3)
-	public static void Feed_3_Ad_Validation() throws Exception {
+	//Execute method for feed_3 Validation
+	public void Feed_3_Ad_Validation() throws Exception {
 		Functions.Feed_Ad_Validation(3);
 	}
 	@Test(priority=4)
-	public static void Feed_4_Ad_VAlidation() throws Exception{
+	//Execute method for feed_4 Validation
+	public void feed_4_Ad_VAlidation() throws Exception{
 		Functions.Feed_Ad_Validation(4);
 	}
 	@Test(priority=5)
-	public static void Feed_5_Ad_Validation() throws Exception {
+	//Execute method for feed_5 Validation
+	public void feed_5_Ad_Validation() throws Exception {
 		Functions.Feed_Ad_Validation(5);
 	}
 	@Test(priority=6)
-	public static void HourlyPageAd_Validations() throws Exception{
+	//Execute method for hourly page ad Validation
+	public void hourlyPageAd_Validations() throws Exception{
 		System.out.println("*********************** Started Validating Hourly Detail page ads ****************");
 		Functions.FeedValue=10;
+		try {
 		//Functions.returnToWeather();
 		//Functions.close_launchApp();
 		Functions.addnewAddress("Detail");
 		//Functions.ScrollUp_ToHomePage();
 		ExecutableFunctions.Function_extened_HourlyPage_ads();
+		}catch(Exception e) {
+			Functions.close_launchApp();
+			Functions.addnewAddress("Detail");
+			//Functions.ScrollUp_ToHomePage();
+			ExecutableFunctions.Function_extened_HourlyPage_ads();
+		}
 	}
 	@Test(priority=7)
-	public static void DailyPageAd_Validations() throws Exception{
+	//Execute method for daily page ad Validation
+	public void dailyPageAd_Validations() throws Exception{
 		System.out.println("*********************** Started Validating Daily Detail page ads ****************");
 		Functions.returnToWeather();
 		ExecutableFunctions.Function_extened_DailyPage_ads();
 	}
 	@Test(priority=8)
-	public static void MapPageAd_Validations()throws Exception{
+	//Execute method for map page ad Validation
+	public void mapPageAd_Validations()throws Exception{
 		System.out.println("*********************** Started Validating Map Detail page ads ****************");
 		Functions.returnToWeather();
 		ExecutableFunctions.Function_extened_MapPage_ads();
 	}
 	@Test(priority=9)
-	public static void NewsPageAd_Validations()throws Exception{
+	//Execute method for news page ad Validation
+	public void newsPageAd_Validations()throws Exception{
 		System.out.println("*********************** Started Validating News Detail page ads ****************");
 		ExecutableFunctions.Function_extened_NewsPage_ads();
 	}
 	@Test(priority=10)
-	public static void HealthPage_cold_Flu_Ad_Validations()throws Exception{
+	//Execute method for Lifestyle combo - Cold & Flu page ad Validation
+	public void healthPage_cold_Flu_Ad_Validations()throws Exception{
 		System.out.println("*********************** Started Validating LS_Cold and Flu Detail page ads ****************");
+		try {
 		Functions.enternewAddress("Bridgeton, New Jersey");
 		Thread.sleep(8000); 
 		ExecutableFunctions.Function_extened_LSModule_ads("LSModule(ColdAndFlu)");
+		}catch(Exception e) {
+			Functions.close_launchApp();
+			Functions.enternewAddress("Bridgeton, New Jersey");
+			Thread.sleep(8000); 
+			ExecutableFunctions.Function_extened_LSModule_ads("LSModule(ColdAndFlu)");
+		}
 	}
 	@Test(priority=11)
-	public static void HealthPage_Allergy_Ad_Validations()throws Exception{
+	//Execute method for Lifestyle combo - Allergy page ad Validation
+	public void healthPage_Allergy_Ad_Validations()throws Exception{
 		System.out.println("*********************** Started Validating Ls_Allergy Detail page ads ****************");
 		ExecutableFunctions.Function_extened_LSModule_ads("LSModule(Allergy)");
 	}
 	@Test(priority=12)
-	public static void HealthPage_Ski_Ad_Validations()throws Exception{
+	//Execute method for Lifestyle combo - Ski page ad Validation
+	public void healthPage_Ski_Ad_Validations()throws Exception{
 		System.out.println("*********************** Started Validating LS_Ski Detail page ads ****************");
 		ExecutableFunctions.Function_extened_LSModule_ads("LSModule(Ski)");
 	}
 	@Test(priority=13)
-	public static void HealthPage_Running_Ad_Validations()throws Exception{
+	//Execute method for Lifestyle combo - Running page ad Validation
+	public void HealthPage_Running_Ad_Validations()throws Exception{
 		System.out.println("*********************** Started Validating LS_Running Detail page ads ****************");
 		ExecutableFunctions.Function_extened_LSModule_ads("LSModule(Running)");
 	}
 	@Test(priority=14)
-	public static void HealthPage_Boat_Beach_Ad_Validations()throws Exception{
+	//Execute method for Lifestyle combo - Boat & Beach page ad Validation
+	public void healthPage_Boat_Beach_Ad_Validations()throws Exception{
 		System.out.println("*********************** Started Validating Ls_Boat_Beach Detail page ads ****************");
 		ExecutableFunctions.Function_extened_LSModule_ads("LSModule(Boat&Beach)");
 	}
 	@Test(priority=15,enabled=false)
-	public static void NativAd_Validations()throws Exception{
+	//Execute method for native ad Validation
+	public void nativAd_Validations()throws Exception{
 		System.out.println("*********************** Started Validating Native ad ****************");
 		Functions.Setappinto_TestMode("Select");
 		Functions.Setappinto_TestMode("nativeAd");
 		ExecutableFunctions.Function_validate_BB_ads("nativeAd");
 	}
-	@Test(priority=16,enabled = false)
-	public static void NativBB_Validations()throws Exception{
+	@Test(priority=16)
+	//Execute method for nativeBB-ad Validation
+	public void nativBB_Validations()throws Exception{
 		System.out.println("*********************** Started Validating Native BB ****************");
 		Functions.Setappinto_TestMode("Select");
 		//Functions.Setappinto_TestMode("nativeBB");
 		ExecutableFunctions.Function_validate_BB_ads("nativeBB");
 	}
-	@Test(priority=17,enabled = false)
-	public static void NativanimatedBB_Validations()throws Exception{
+	@Test(priority=17)
+	//Execute method for native_animated BB  ad Validation
+	public void nativanimatedBB_Validations()throws Exception{
 		System.out.println("*********************** Started Validating Native animated BB ****************");
 		//Functions.Setappinto_TestMode("native_animated_BB");
 		ExecutableFunctions.Function_validate_BB_ads("native_animated_BB");
 	}
 	@Test(priority=18,enabled = false)
-	public static void staticBBAd_Validations()throws Exception{
+	//Execute method for Static BB Ad Validation
+	public void staticBBAd_Validations()throws Exception{
 		System.out.println("*********************** Started Validating StaticBB ad ****************");
 		//Functions.Setappinto_TestMode("Select");
 		ExecutableFunctions.Function_validate_BB_ads("StaticBB");
 	}
 	
 	@Test(priority=19,enabled = false)
-	public static void animatedBBAd_Validations()throws Exception{
+	//Execute method for animated BB Ad Validation
+	public void animatedBBAd_Validations()throws Exception{
 		System.out.println("*********************** Started Validating AnimatedBB ad ****************");
 		ExecutableFunctions.Function_validate_BB_ads("AnimatedBB");
 	}
@@ -146,6 +189,7 @@ public class AdMonitor_run extends Driver {
 	}
 
 	@BeforeTest
+	//before test 
 	public void beforeTest() throws Exception {
 		//Preconditions
 		Functions.startCharlesSession();
