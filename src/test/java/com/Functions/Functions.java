cd9ecaa7fd6a470897f590ab17344c8036495172
naresh.kumar.contractor@weather.com <naresh.kumar.contractor@weather.com>
2018-02-07 20:54:55 +0530

    scroll increased for BB
    scroll increased for BB

--- src/test/java/com/Functions/Functions.java
+++ src/test/java/com/Functions/Functions.java
@@ -3253,11 +3253,11 @@ public class Functions extends Driver{
 			}
 
 			Functions.Navigate_extendedPages(Pagename);
-			if(Pagename.equals("LSModule(Ski)")||Pagename.equals("LSModule(Allergy)")||Pagename.equals("LSModule(Cold & Flu)")||Pagename.equals("LSModule(Boat & Beach)")){
+			if(Pagename.equals("LSModule(Ski)")||Pagename.equals("LSModule(Allergy)")||Pagename.equals("LSModule(Cold & Flu)")){
 				Functions.Swipe();
 				Functions.Swipe();				
 				System.out.println("Page scrolled for add");
-			}else if(Pagename.equals("LSModule(Running)")){
+			}else if(Pagename.equals("LSModule(Running)")||Pagename.equals("LSModule(Boat & Beach)")){
 				Functions.Swipe();
 				Functions.Swipe();
 				Functions.Swipe();
